00b186736764c0ff2ca68055729515830bc11ab0
Avelar Luna Juan Pablo (BGSW/EGB1-MS) <eva9ga@bosch.com>
2022-04-18 18:34:42 -0500

    fix sensors sender7

--- input_sensor_data.py
+++ input_sensor_data.py
@@ -7,12 +7,12 @@ with open('Input_example.csv', newline='') as csvfile:
     filereader = csv.reader(csvfile, delimiter=',')
     start_read = False
     for row in filereader:
-        if row == "END":
+        if row == 'END':
             start_read = False
         if start_read == True:
             A2D_readings.append(row[0])
             temperature_sensor_readings.append(row[1])
             print(f'element1:{row[0]}, element2:{row[1]}')
-        if row == "START":
+        if row == 'START':
             start_read = True
         print(f'element1:{row[0]}')
