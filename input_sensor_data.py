20572d637eebd17d56ee9bb0d96561679f697507
Avelar Luna Juan Pablo (BGSW/EGB1-MS) <eva9ga@bosch.com>
2022-04-18 17:45:40 -0500

    The workflow has been added2

--- input_sensor_data.py
+++ input_sensor_data.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('input_example.csv', newline='') as csvfile:
+with open('Input_example.csv', newline='') as csvfile:
     filereader = csv.reader(csvfile)
     for row in filereader:
         print(row)
