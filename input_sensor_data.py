6555bb858196aab047bbd1b0b74df6e37a04410d
Avelar Luna Juan Pablo (BGSW/EGB1-MS) <eva9ga@bosch.com>
2022-04-18 18:10:51 -0500

    fix sensors sender

--- input_sensor_data.py
+++ input_sensor_data.py
@@ -1,6 +1,11 @@
 import csv
 
+A2D_readings = []
+temperature_sensor_readings = []
+
 with open('Input_example.csv', newline='') as csvfile:
     filereader = csv.reader(csvfile)
     for row in filereader:
-        print(row)
+        A2D_readings.append(row[0])
+        temperature_sensor_readings.append(row[1])
+        print(f'element1:{row[0]}, element2:{row[1]}')
