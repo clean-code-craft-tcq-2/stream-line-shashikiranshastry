f26e45769ba88678b5aff94fcb3f462a03ce70af
shashikiranshastry <91182428+shashikiranshastry@users.noreply.github.com>
2022-05-06 09:19:36 +0530

    Update testdata.py

--- testdata.py
+++ testdata.py
@@ -10,6 +10,6 @@ class sender_test(unittest.TestCase):
   def test(self):
     for test_data in range (len(input_sensor_data.test_A2D_readings)):
       self.assertEqual(sender.start_sending(input_sensor_data.test_A2D_readings[test_data],input_sensor_data.test_temperature_readings[test_data]), input_sensor_data.result_data[test_data] , "not equal")
-      print ("--------------------------")
+      print ("-")
         
 unittest.main()
